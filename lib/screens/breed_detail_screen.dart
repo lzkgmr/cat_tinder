cb74055b53a7c2c5b66a5469ccec02fd1f4719db
lzkgmr <lzkgmr@gmail.com>
2025-12-04 16:19:49 +0300

    added pretty part of breed's description

--- lib/screens/breed_detail_screen.dart
+++ lib/screens/breed_detail_screen.dart
@@ -1,38 +1,121 @@
 import 'package:flutter/material.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import '../models/breed.dart';
 
 class BreedDetailScreen extends StatelessWidget {
   final Breed breed;
-  const BreedDetailScreen({super.key, required this.breed});
+  final Widget imageWidget;
+  const BreedDetailScreen({super.key, required this.breed, required this.imageWidget});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(breed.name)),
+      backgroundColor: Color.fromARGB(255, 242, 242, 242),
+      appBar: AppBar(title: Text(breed.name, style: const TextStyle(fontWeight: FontWeight.w600)), backgroundColor: Color.fromARGB(255, 242, 242, 242)),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            if (breed.imageUrl != null)
-              CachedNetworkImage(
-                imageUrl: breed.imageUrl!,
-                width: double.infinity,
-                height: 250,
-                fit: BoxFit.cover,
-                placeholder: (_, _) => const CircularProgressIndicator(),
-                errorWidget: (_, _, _) => const Icon(Icons.error),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: imageWidget,
+            ),
+            const SizedBox(height: 16),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
               ),
-            const SizedBox(height: 16),
-            Text(breed.description ?? 'No description', style: const TextStyle(fontSize: 16)),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                _buildCharacteristic('Adaptability', breed.adaptability),
-                _buildCharacteristic('Intelligence', breed.intelligence),
-              ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Bio',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink[100],
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    breed.description ?? 'No description',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 10),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Adaptability',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink[100],
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: List.generate(5, (index) {
+                      int rating = breed.adaptability ?? 0;
+                      return Icon(
+                        Icons.star,
+                        color: index < rating ? Colors.amber : Colors.grey[300],
+                        size: 24,
+                      );
+                    }),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Intelligence',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.pink[100],
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: List.generate(5, (index) {
+                      int rating = breed.intelligence ?? 0;
+                      return Icon(
+                        Icons.star,
+                        color: index < rating ? Colors.amber : Colors.grey[300],
+                        size: 24,
+                      );
+                    }),
+                  ),
+                ],
+              ),
             ),
             const SizedBox(height: 8),
             Row(
